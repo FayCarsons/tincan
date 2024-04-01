4af23a9d24640a9ecd3d80cdc75d3437f9c4e06d
Fay Carsons <faycarsons23@gmail.com>
2024-04-01 13:33:08 -0400

    add config page and input validation

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ and Mint Tea for terminal UI
 To run: pin Riot to trunk and Minttea(and its related libraries Spices and Leaves)
 to [this branch](https://github.com/leostera/minttea/tree/feat/upgrade-to-riot-30-03) and run `dune exec --release _build/default/bin/main.exe` 
 
-There are no command line args, setup is entirely handled by the TUI currently.
+There are no command line args, setup (including basic validation of input) is entirely handled by the TUI.
 I may add the option to skip straight to the mode (host/client) of your choice later.
 
 Can be laggy in dev builds, best built with the `--release` flag
--- test/test_server.ml
+++ test/test_server.ml
@@ -4,22 +4,21 @@ module Utils = Chat.Utils
 module Tui = Chat.Tui
 
 (*
-    Unfortunately, due to Riot's multi-threading and scheduler system,
-    testing the Server and some of the TUI 
-    is highly involved, and I haven't had time
-    to get the coverage where I want it
+   Unfortunately, due to Riot's multi-threading and scheduler system,
+   testing the Server and some of the TUI
+   is highly involved, and I haven't had time
+   to get the coverage where I want it
 *)
 
-let test_acknowledged _ =
-  assert (String.equal Server.Connection.acknowleged "$PING")
+let test_acknowledged _ = assert (String.equal Server.Connection.acknowleged "$PING")
 
 let server_suite =
-  "TCP_server_tests"
-  >::: [ "test acknowleged flag equality works" >:: test_acknowledged ]
+  "TCP_server_tests" >::: [ "test acknowleged flag equality works" >:: test_acknowledged ]
+;;
 
 let () = OUnit2.run_test_tt_main server_suite
 
-(** tests both the update fn output for a given idx and the `bound_idx` function used to 
+(** tests both the update fn output for a given idx and the `bound_idx` function used to
     restrict indices to valid menu options *)
 let test_menu_idx _ =
   let open Minttea in
@@ -34,19 +33,19 @@ let test_menu_idx _ =
   assert_equal
     (update (Event.KeyDown (Event.Down, No_modifier)) (Menu 1))
     (Menu 0, Command.Noop)
+;;
 
 (* Test TUI's responses to messages sent server->tui *)
 let test_custom_events _ =
   let open Minttea in
   let open Tui in
   let init_state =
-    {
-      role = Host;
-      connection = Open Riot.Pid.zero;
-      acknowleged = HostAcknowleged;
-      text = Leaves.Text_input.empty ();
-      spinner = None;
-      history = [];
+    { role = Host
+    ; connection = Open Riot.Pid.zero
+    ; acknowleged = HostAcknowleged
+    ; text = Leaves.Text_input.empty ()
+    ; spinner = None
+    ; history = []
     }
   in
   assert_equal
@@ -54,40 +53,35 @@ let test_custom_events _ =
     (Chat init_state, Command.Noop);
   assert_equal
     (update (Event.Custom (Utils.Err "oops")) (Chat init_state))
-    ( Chat { init_state with history = [ (init_state.role, "oops") ] },
-      Command.Noop );
+    (Chat { init_state with history = [ init_state.role, "oops" ] }, Command.Noop);
   assert_equal
     (update (Event.Custom (Utils.Received "hi")) (Chat init_state))
-    ( Chat { init_state with history = [ (not_role init_state.role, "hi") ] },
-      Command.Noop );
+    (Chat { init_state with history = [ not_role init_state.role, "hi" ] }, Command.Noop);
   assert_equal
     (update (Event.Custom Utils.Acknowleged) (Chat init_state))
     (Chat init_state, Command.Noop);
   assert_equal
-    (update (Event.Custom (Utils.Connected Riot.Pid.zero))
+    (update
+       (Event.Custom (Utils.Connected Riot.Pid.zero))
        (Chat { init_state with connection = Waiting }))
-    ( Chat
-        {
-          init_state with
-          history = [ (Host, "Established connection with client") ];
-        },
-      Command.Noop );
+    ( Chat { init_state with history = [ Host, "Established connection with client" ] }
+    , Command.Noop );
   assert_equal
     (update (Event.Custom Utils.Closed) (Chat init_state))
     (Chat { init_state with connection = Shutdown }, Command.Noop)
+;;
 
 (* Test key down events, not full coverage as some require a server process to be running *)
 let test_key_events _ =
   let open Minttea in
   let open Tui in
   let init_state =
-    {
-      role = Host;
-      connection = Waiting;
-      acknowleged = HostAcknowleged;
-      text = Leaves.Text_input.empty ();
-      spinner = None;
-      history = [];
+    { role = Host
+    ; connection = Waiting
+    ; acknowleged = HostAcknowleged
+    ; text = Leaves.Text_input.empty ()
+    ; spinner = None
+    ; history = []
     }
   in
   assert_equal
@@ -99,43 +93,43 @@ let test_key_events _ =
   match with_char with
   | Chat { text; _ } -> assert_equal (Leaves.Text_input.current_text text) "a"
   | _ -> failwith "Char not added to text buffer"
+;;
 
 (* Tests spinner updates that happen in response Event.Frame events *)
 let test_frame _ =
   let open Minttea in
   let open Tui in
   let init_state =
-    {
-      role = Host;
-      connection = Open Riot.Pid.zero;
-      acknowleged = HostAcknowleged;
-      text = Leaves.Text_input.empty ();
-      spinner = None;
-      history = [];
+    { role = Host
+    ; connection = Open Riot.Pid.zero
+    ; acknowleged = HostAcknowleged
+    ; text = Leaves.Text_input.empty ()
+    ; spinner = None
+    ; history = []
     }
   in
   let updated, _ = update (Event.Frame Ptime.min) (Chat init_state) in
   (match updated with
-  | Chat { spinner; _ } -> assert (Option.is_none spinner)
-  | _ -> failwith "Spinner created during update");
+   | Chat { spinner; _ } -> assert (Option.is_none spinner)
+   | _ -> failwith "Spinner created during update");
   let with_spinner = { init_state with spinner = Some Leaves.Spinner.dot } in
   let updated, _ = update (Event.Frame Ptime.min) (Chat with_spinner) in
   let original, updated =
-    match (with_spinner, updated) with
-    | og, Chat updated
-      when Option.is_some og.spinner && Option.is_some updated.spinner ->
-        (Option.get og.spinner, Option.get updated.spinner)
+    match with_spinner, updated with
+    | og, Chat updated when Option.is_some og.spinner && Option.is_some updated.spinner ->
+      Option.get og.spinner, Option.get updated.spinner
     | _ -> failwith "Spinners nott properly updated"
   in
   assert_bool "Spinner changed pattern during update" (original == updated)
+;;
 
 let tui_suite =
   "Terminal_UI_tests"
-  >::: [
-         "Test menu updates" >:: test_menu_idx;
-         "Test chat upddate" >:: test_custom_events;
-         "Test KeyDown events" >:: test_key_events;
-         "Test Frame update" >:: test_frame;
+  >::: [ "Test menu updates" >:: test_menu_idx
+       ; "Test chat upddate" >:: test_custom_events
+       ; "Test KeyDown events" >:: test_key_events
+       ; "Test Frame update" >:: test_frame
        ]
+;;
 
 let () = OUnit2.run_test_tt_main tui_suite
